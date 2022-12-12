267f102a07fcd5ee0327460d816ebc12395117a8
Stephen Ray <stephenray@sr2610.com>
2022-12-12 22:23:48 +0000

    Optimise Memory Issue in Day 12 a bit

--- Solutions/Day12.cs
+++ Solutions/Day12.cs
@@ -7,42 +7,44 @@ namespace Advent_2022.Solutions
 {
     public static class Day12
     {
-        private static Dictionary<Position, HashSet<Position>> AdjacencyList { get; } =
-            new Dictionary<Position, HashSet<Position>>();
+        private static Dictionary<Tuple<int, int>, HashSet<Tuple<int, int>>> AdjacencyList { get; } =
+            new Dictionary<Tuple<int, int>, HashSet<Tuple<int, int>>>();
+
 
         public static int SolvePartOne(List<string> data)
         {
             AdjacencyList.Clear();
-            GenerateMap(data, out Position startPos, out Position endPos);
+            GenerateMap(data, out var startPos, out var endPos);
             return GetShortestPath(startPos, endPos);
         }
 
         [SuppressMessage("ReSharper.DPA", "DPA0001: Memory allocation issues")]
-        private static int GetShortestPath(Position startPos, Position endPos)
+        private static int GetShortestPath(Tuple<int, int> startPos, Tuple<int, int> endPos)
         {
-            var previous = new Dictionary<Position, Position>();
-
-            var queue = new Queue<Position>();
+            var previous = new Dictionary<Tuple<int, int>, Tuple<int, int>>();
+            var queue = new Queue<Tuple<int, int>>();
             queue.Enqueue(startPos);
 
             while (queue.Count > 0)
             {
-                Position vertex = queue.Dequeue();
-                foreach (Position neighbor in AdjacencyList[vertex].Where(neighbor => !previous.ContainsKey(neighbor)))
-                {
-                    previous[neighbor] = vertex;
-                    queue.Enqueue(neighbor);
-                }
+                var vertex = queue.Dequeue();
+
+                foreach (var neighbor in AdjacencyList[vertex])
+                    if (!previous.ContainsKey(neighbor))
+                    {
+                        previous[neighbor] = vertex;
+                        queue.Enqueue(neighbor);
+                    }
             }
 
-            var path = new List<Position>();
+            var path = new List<Tuple<int, int>>();
 
-            Position current = endPos;
+            var current = endPos;
             while (!current.Equals(startPos))
             {
                 path.Add(current);
-                if(previous.ContainsKey(current))
-                 current = previous[current];
+                if (previous.ContainsKey(current))
+                    current = previous[current];
                 else
                     return int.MaxValue;
             }
@@ -52,12 +54,13 @@ namespace Advent_2022.Solutions
             return path.Count() - 1;
         }
 
-        private static HashSet<Position> GenerateMap(List<string> data, out Position startPos, out Position endPos)
+        private static HashSet<Tuple<int, int>> GenerateMap(List<string> data, out Tuple<int, int> startPos,
+            out Tuple<int, int> endPos)
         {
-            var lowestPoints = new HashSet<Position>();
+            var lowestPoints = new HashSet<Tuple<int, int>>();
             int[,] map = new int[data.First().Length, data.Count()];
-            startPos = new Position(0, 0);
-            endPos = new Position(0, 0);
+            startPos = new Tuple<int, int>(0, 0);
+            endPos = new Tuple<int, int>(0, 0);
             for (int y = 0; y < data.Count(); y++)
             for (int x = 0; x < data[y].Length; x++)
             {
@@ -65,15 +68,15 @@ namespace Advent_2022.Solutions
                 switch (currentPoint)
                 {
                     case 'S':
-                        startPos = new Position(x, y);
+                        startPos = new Tuple<int, int>(x, y);
                         map[x, y] = 0;
                         break;
                     case 'E':
-                        endPos = new Position(x, y);
+                        endPos = new Tuple<int, int>(x, y);
                         map[x, y] = 26;
                         break;
                     case 'a':
-                        lowestPoints.Add(new Position(x, y));
+                        lowestPoints.Add(new Tuple<int, int>(x, y));
                         map[x, y] = char.ToUpper(currentPoint) - 64;
                         break;
                     default:
@@ -89,21 +92,21 @@ namespace Advent_2022.Solutions
             for (int y = 0; y < height; y++)
             for (int x = 0; x < width; x++)
             {
-                var adjacent = new HashSet<Position>();
+                var adjacent = new HashSet<Tuple<int, int>>();
 
                 if (x > 0 && map[x - 1, y] <= map[x, y] + 1)
-                    adjacent.Add(new Position(x - 1, y));
+                    adjacent.Add(new Tuple<int, int>(x - 1, y));
                 if (x < width - 1 && map[x + 1, y] <= map[x, y] + 1)
-                    adjacent.Add(new Position(x + 1, y));
+                    adjacent.Add(new Tuple<int, int>(x + 1, y));
 
 
                 if (y > 0 && map[x, y - 1] <= map[x, y] + 1)
-                    adjacent.Add(new Position(x, y - 1));
+                    adjacent.Add(new Tuple<int, int>(x, y - 1));
 
                 if (y < height - 1 && map[x, y + 1] <= map[x, y] + 1)
-                    adjacent.Add(new Position(x, y + 1));
+                    adjacent.Add(new Tuple<int, int>(x, y + 1));
 
-                AdjacencyList.Add(new Position(x, y), adjacent);
+                AdjacencyList.Add(new Tuple<int, int>(x, y), adjacent);
             }
 
             return lowestPoints;
@@ -112,15 +115,16 @@ namespace Advent_2022.Solutions
         public static int SolvePartTwo(List<string> data)
         {
             AdjacencyList.Clear();
-            var points = GenerateMap(data, out Position startPos, out Position endPos);
+            var points = GenerateMap(data, out var startPos, out var endPos);
 
 
             int shortestDistance = int.MaxValue;
-            foreach (Position lowPoint in points)
+
+            foreach (var lowPoint in points)
             {
                 int dist = GetShortestPath(lowPoint, endPos);
                 if (dist < shortestDistance)
-                   shortestDistance = dist;
+                    shortestDistance = dist;
             }
 
 
